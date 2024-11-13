5d737f9a3c09bf3047d3f13d37ec3d01fd09c5bb
ttttkii913 <dbgkdud821@daum.net>
2024-11-13 20:41:52 +0900

    Chore: init

---
+++ pallete/src/test/java/org/pallete/PalleteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pallete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalleteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

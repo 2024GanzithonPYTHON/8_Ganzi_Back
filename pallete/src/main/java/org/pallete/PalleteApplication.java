ba33a6129b8f27114fb49d958c4768fdf1fed4b3
Gyoseung Ku <ku@gyoseung.me>
2024-11-17 11:57:30 +0900

    Connect Database
    .env 파일 활용 바랍니다.

--- pallete/src/main/java/org/pallete/PalleteApplication.java
+++ pallete/src/main/java/org/pallete/PalleteApplication.java
@@ -1,13 +1,22 @@
 package org.pallete;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PalleteApplication {
 
+    @PostConstruct
+    public void loadEnv() {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        System.setProperty("spring.datasource.url", dotenv.get("DB_URL"));
+        System.setProperty("spring.datasource.username", dotenv.get("DB_USERNAME"));
+        System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(PalleteApplication.class, args);
     }
-
 }

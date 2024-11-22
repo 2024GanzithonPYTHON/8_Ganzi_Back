14b82c7434ef3cb92d19d274c083f8c4fa3c4317
ttttkii913 <dbgkdud821@daum.net>
2024-11-22 17:49:27 +0900

    Feat: 유효성 검사 추가

--- pallete/src/main/java/org/pallete/diary/domain/dto/diaryDto/DiaryRequestDto.java
+++ pallete/src/main/java/org/pallete/diary/domain/dto/diaryDto/DiaryRequestDto.java
@@ -2,6 +2,7 @@ package org.pallete.diary.domain.dto.diaryDto;
 
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,6 +16,7 @@ public class DiaryRequestDto {
     private String title;
 
     @NotNull
+    @Size(max = 500, message = "500자 이상 작성할 수 없습니다.")
     private String content;
 
     private boolean isVisible;
--- pallete/src/main/java/org/pallete/login/model/UserSignUpReqDto.java
+++ pallete/src/main/java/org/pallete/login/model/UserSignUpReqDto.java
@@ -1,9 +1,28 @@
 package org.pallete.login.model;
 
-public record UserSignUpReqDto(
-        String email,
-        String name,
-        String password
-) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
+// 회원가입
+public record UserSignUpReqDto(
+        @NotBlank(message = "이메일은 필수로 입력해야 합니다.")
+        @Pattern(regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$",
+                message = "이메일 형식에 맞지 않습니다.")
+        String email,
+
+        @NotBlank(message = "이름은 필수로 입력해야 합니다.")
+        @Size(min = 2, message = "이름은 최소 2자 이상 입력해야 합니다.")
+        String name,
+
+        @NotBlank(message = "비밀번호는 필수로 입력해야 합니다.")
+        @Size(min = 8, message = "비밀번호는 최소 8자 이상이어야 합니다.")
+        String password,
+
+        @NotBlank(message = "비밀번호 확인은 필수입니다.")
+        String checkPassword
+) {
+    public boolean passwordsMatch() {
+        return password.equals(checkPassword);
+    }
 }
